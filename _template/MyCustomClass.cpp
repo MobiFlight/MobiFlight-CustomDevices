8f4cf88c5b12eb95be10d138dd59276c7403a9b3
elral <3263285+elral@users.noreply.github.com>
2024-01-14 13:30:47 +0100

    Adding Power saving mode (#42)
    * -2 will not be sent, comments and functions adapted
    
    * -2 will not be send, comments and functions adapted
    
    * PowerSavingMode can be enteres or leaved
    
    * MessageID '-2' for PowerSavingMode, '-1' for Stop

--- _template/MyCustomClass.cpp
+++ _template/MyCustomClass.cpp
@@ -35,33 +35,33 @@ void MyCustomClass::set(int16_t messageID, char *setPoint)
         Each messageID has it's own value
         check for the messageID and define what to do.
         Important Remark!
-        MessageID == -1 will be send from the connector when Mobiflight is closed
-        Put in your code to shut down your custom device (e.g. clear a display)
-        MessageID == -2 will be send from the connector when PowerSavingMode is entered
+        MessageID == -2 will be send from the board when PowerSavingMode is set
+            Message will be "0" for leaving and "1" for entering PowerSavingMode
+        MessageID == -1 will be send from the connector when Connector stops running
+            Message will be "0" for leaving and "1" for entering PowerSavingMode
         Put in your code to enter this mode (e.g. clear a display)
-
     ********************************************************************************** */
     int32_t  data = atoi(setPoint);
     uint16_t output;
 
     // do something according your messageID
     switch (messageID) {
-    case -1:
-        // tbd., get's called when Mobiflight shuts down
-    case -2:
-        // tbd., get's called when PowerSavingMode is entered
-    case 0:
-        output = (uint16_t)data;
-        data   = output;
-        break;
-    case 1:
-        /* code */
-        break;
-    case 2:
-        /* code */
-        break;
-    default:
-        break;
+        case -2:
+            // tbd., get's called when PowerSavingMode is set
+        case -1:
+            // tbd., get's called when Connector stops running
+        case 0:
+            output = (uint16_t)data;
+            data   = output;
+            break;
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        default:
+            break;
     }
 }
 
@@ -69,3 +69,4 @@ void MyCustomClass::update()
 {
     // Do something which is required regulary
 }
+
